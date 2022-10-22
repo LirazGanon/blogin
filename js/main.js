f6f411a76efbc86eaf1e4ad55e7d9cb61f3a076b
לירז גנון <lirazganon@gmail.com>
2022-10-22 19:04:38 +0300

    design fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 'use strict'
 
-function toggleMenu(){
+function oninit() {
+    let elCurrLink = document.querySelector('.li-nav-link')
+    const elNavLink = document.querySelectorAll('.li-nav-link')
+    elNavLink.forEach(link => link.addEventListener('click',(e)=>{
+        elCurrLink.classList.remove('current-page-bottom')
+        elCurrLink.firstChild.classList.remove('current-page-color')
+        elCurrLink = (e.target.tagName === 'A')? e.target.parentElement: e.target 
+        elCurrLink.classList.add('current-page-bottom')
+        elCurrLink.firstChild.classList.add('current-page-color')
+    }
+    ))
+}
+
+function toggleMenu() {
     document.body.classList.toggle('menu-open')
 }
